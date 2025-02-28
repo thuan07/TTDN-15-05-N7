818168038478939529309469c4595f3e525c9ee4
DoLinh2308 <dotronglinhhh@gmail.com>
2025-02-28 09:12:39 +0700

    Update

--- addons/du_an/models/project.py
+++ addons/du_an/models/project.py
@@ -17,30 +17,9 @@ class Project(models.Model):
     employee_ids = fields.Many2many("nhan_vien", string="Nhân viên tham gia")
     task_ids = fields.One2many("project_task", "project_id", string="Danh sách nhiệm vụ")
 
-    # Thống kê nhiệm vụ
-    total_tasks = fields.Integer("Tổng số nhiệm vụ", compute="_compute_task_counts", store=True)
-    done_tasks = fields.Integer("Số nhiệm vụ hoàn thành", compute="_compute_task_counts", store=True)
-    progress = fields.Float("Tiến độ (%)", compute="_compute_progress", store=True)
-
-    @api.depends('task_ids.status')
-    def _compute_task_counts(self):
-        for project in self:
-            tasks = project.task_ids
-            project.total_tasks = len(tasks)
-            project.done_tasks = len(tasks.filtered(lambda t: t.status == 'done'))
-
-    @api.depends('total_tasks', 'done_tasks')
-    def _compute_progress(self):
-        for project in self:
-            if project.total_tasks > 0:
-                project.progress = (project.done_tasks / project.total_tasks) * 100
-            else:
-                project.progress = 0
-
     log_ids = fields.One2many('project_log', 'project_id', string="Nhật ký hoạt động")
 
     def write(self, vals):
-        """ Ghi log khi trạng thái dự án thay đổi """
         if 'status' in vals:
             for project in self:
                 status_mapping = {
